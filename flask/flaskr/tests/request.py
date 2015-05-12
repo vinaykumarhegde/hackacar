7bfb188df2afe18ea7989b164aa4023b9b9f39c1
Robert Fornof <robert4nof@gmail.com>
2015-05-12 16:37:44 -0700

    following tut: https://developer.moj.io/university/yourfirstapp

--- flask/flaskr/tests/request.py
+++ flask/flaskr/tests/request.py
@@ -1,10 +1,19 @@
 # request.py
 import requests , json
 
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO
+
+
 headers = {'MojioAPIToken': '6ae1f07c-6db7-4a49-b458-2a1ffdfdc52e'}
 r = requests.get("https://api.moj.io:443/v1/Events/", headers=headers)
 
 print r
-print r.json()[Events]
+print r.json()
 
+#observers
+# socket.io 
+# signalr ---> backend connection : 
 r = requests.get("https://api.moj.io:443/v1/Events/", headers=headers)
+
+
